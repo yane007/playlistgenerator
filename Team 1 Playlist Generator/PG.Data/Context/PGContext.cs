536cf1b6fa1113f8b72583a083a69a5e9fc77908
Yane Yankov <yankov.yane@gmail.com>
2020-11-02 15:09:03 +0200

    Implemented project skeleton and installed NuGet packages

---
+++ Team 1 Playlist Generator/PG.Data/Context/PGContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PG.Models;
+using System;
+
+namespace PG.Data.Context
+{
+    public class PGContext //Add Identity
+    {
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Playlist> Playlists { get; set; }
+        public DbSet<Song> Songs { get; set; }
+    }
+}
---
+++ Team 1 Playlist Generator/PG.Models/Song.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PG.Models
+{
+    public class Song
+    {
+    }
+}
