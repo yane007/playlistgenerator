054bdbb437c704674a67910a178725c0ec5fc9a2
georgi stoichkov <jory1@mail.bg>
2020-11-02 19:13:26 +0200

    DTO's Added without UsersDTO

---
+++ Team 1 Playlist Generator/PG.Services/DTOs/ArtistDTO.cs
@@ -0,0 +1,14 @@
+﻿using PG.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PG.Services.DTOs
+{
+    public class ArtistDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Song> Songs { get; set; } = new List<Song>();
+    }
+}
---
+++ Team 1 Playlist Generator/PG.Services/DTOs/PlaylistDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PG.Models;
+
+namespace PG.Services.DTOs
+{
+    public class PlaylistDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Duration { get; set; }
+        public int Rank { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        public ICollection<Song> Songs { get; set; } = new List<Song>();
+
+    }
+}
