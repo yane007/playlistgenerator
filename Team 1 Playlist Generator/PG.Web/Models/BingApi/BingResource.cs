9f466e3b7b7aec68c1a2929953e0ccd848fcdb4f
georgi stoichkov <jory1@mail.bg>
2020-11-13 13:37:24 +0200

    Check Commit

--- Team 1 Playlist Generator/PG.Web/Models/BingApi/BingResource.cs
+++ Team 1 Playlist Generator/PG.Web/Models/BingApi/BingResource.cs
@@ -7,6 +7,7 @@ namespace DeezerApiData.Models.BingApi
     public class BingResource
     {
         [JsonPropertyName("resources")]
+
         public IEnumerable<TravelResultsCollection> TravelResultsCollections { get; set; }
     }
 }
